eebf648a6f325bcb2e6c9e9f6228928f5bd7d6e1
Prathyusha <siyalaprathyu2005@gmail.com>
2026-07-29 23:43:12 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll("nav a");
-
-links.forEach(link => {
-    link.addEventListener("click", function () {
-        alert("Opening " + this.innerText + " Section");
-    });
-});
